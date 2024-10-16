fc481231ee442a331c27a9bf0be8d946263b7b82
Muhammad Irfan Khan <hammadbd08@gmail.com>
2024-10-16 19:45:54 +0500

    updated_nointern

--- lib/widgets/no_internet_dialog.dart
+++ lib/widgets/no_internet_dialog.dart
@@ -13,54 +13,56 @@ class NoInternetDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      contentPadding: EdgeInsets.zero,
-      backgroundColor: AColors.white.withOpacity(.7),
-      content: Stack(
-        children: [
-          ARoundedContainer(
-            padding: 20,
-            bgColor: Colors.transparent,
-            height: 300,
-            child: Column(
-              children: [
-                const Image(
-                  image: AssetImage("assets/noInternet.png"),
-                  height: 100,
-                ),
-                const Text(
-                  textAlign: TextAlign.center,
-                  "Oh No!",
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: AColors.dark,
-                      fontWeight: FontWeight.w600),
-                ),
-                const Text(
+    return PopScope(
+     canPop:false ,
+      child: AlertDialog(
+        contentPadding: EdgeInsets.zero,
+        backgroundColor: AColors.white.withOpacity(.7),
+        content: Stack(
+          children: [
+            ARoundedContainer(
+              padding: 20,
+              bgColor: Colors.transparent,
+              height: 300,
+              child: Column(
+                children: [
+                  const Image(
+                    image: AssetImage("assets/noInternet.png"),
+                    height: 100,
+                  ),
+                  const Text(
                     textAlign: TextAlign.center,
-                    "No internet connection found Check your connection and try again"),
-                const Spacer(),
-                AElevatedButton(
-                    bgColor: AColors.primary,
-                    title: "Try Again",
-                    onPress: onPress)
-              ],
+                    "Oh No!",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: AColors.dark,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const Text(
+                      textAlign: TextAlign.center,
+                      "No internet connection found Check your connection and try again"),
+                  const Spacer(),
+                  AElevatedButton(
+                      bgColor: AColors.primary,
+                      title: "Try Again",
+                      onPress: onPress)
+                ],
+              ),
             ),
-          ),
-          // Positioned(
-          //   top: 0,
-          //   right: 0,
-          //   child: IconButton(
-          //     onPressed: () {
-          //       Get.back();
-          //     },
-          //     icon: const Icon(
-          //       Icons.close,
-          //       color: AColors.dark,
-          //     ),
-          //   ),
-          // )
-        ],
+            // Positioned(
+            //   top: 0,
+            //   right: 0,
+            //   child: IconButton(
+            //     onPressed: () {
+            //       Get.back();
+            //     },
+            //     icon: const Icon(
+            //       Icons.close,
+            //       color: AColors.dark,
+            //     ),
+            //   )
+          ],
+        ),
       ),
     );
   }
